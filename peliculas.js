2557764724a708656a4a7b155e10bfe84b783e6a
celsosoria-byte <celso.soria@anima.edu.uy>
2026-04-27 20:17:49 -0300

    peliculas.js

---
+++ peliculas.js
@@ -0,0 +1,8 @@
+const pelicula = {
+  titulo: "Volver al Futuro",
+  anio: 1985,
+  director: "Robert Zemeckis",
+  disponible: true,
+  generos: ["comedia", "ciencia ficcion", "aventura"],
+};
+console.log(pelicula);
---
+++ peliculas2.js
@@ -0,0 +1,8 @@
+const futbol = {
+  titulo: "ronaldo",
+  anio: 1994,
+  director: "zagallo",
+  disponible: true,
+  generos: ["futbol", "pelota", "gol"],
+};
+console.log(futbol.director);
